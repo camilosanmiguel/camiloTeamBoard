ee289d2bd794e1ad28a11edc73fe5dba7d2e2d2d
Camilo Sanmiguel <camilo.sanmiguel@gmail.com>
2021-08-26 16:00:41 -0500

    finish regis user

--- frontend/src/app/home/register/register.component.ts
+++ frontend/src/app/home/register/register.component.ts
@@ -13,19 +13,67 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  registerData:any;
+  registerData: any;
   message: string;
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationSeconds: number = 2000;
 
   constructor(
-    private userServ: UserService,
-    private router: Router,
-    private snackBar: MatSnackBar
+    private _userServ: UserService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
   ) {
     this.registerData = {};
-    this.message = ''
+    this.message = '';
   }
 
   ngOnInit(): void {}
+
+  registerUser() {
+    if (
+      !this.registerData.name ||
+      !this.registerData.email ||
+      !this.registerData.password
+    ) {
+      console.log('Failed process: Imcomplete data');
+      this.message = 'Failed process: Imcomplete data';
+      this.openSnackBarError();
+      this.registerData = {};
+    } else {
+      this._userServ.registerUser(this.registerData).subscribe(
+        (res) => {
+          console.log(res);
+          localStorage.setItem('token', res.jwtToken);
+          this._router.navigate(['/saveTask']);
+          this.message = 'Sucessfull user registration';
+          this.openSnackBarSuccesfull();
+          this.registerData = {};
+        },
+        (err) => {
+          console.log(err);
+          this.message = err.error;
+          this.openSnackBarError();
+        }
+      );
+    }
+  }
+
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationSeconds,
+      panelClass: ['style-snackBarTrue'],
+    });
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationSeconds,
+      panelClass: ['style-snackBarFalse'],
+    });
+  }
 }
